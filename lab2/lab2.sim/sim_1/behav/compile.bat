95d5c48a1a5e260d7f2c2f7584a40d02fc3eaabc
Rutheniumlmw <1791016517@qq.com>
2022-11-07 01:39:56 +0800

    first commit

--- lab2/lab2.sim/sim_1/behav/compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set xv_path=D:\\studysourcefile\\verilog\\vivado\\Vivado\\2015.2\\bin
-if "%errorlevel%"=="1" goto END
-if "%errorlevel%"=="0" goto SUCCESS
-:END
-exit 1
-:SUCCESS
-exit 0
